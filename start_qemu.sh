8ca030a4eedf7cb6ec1a09bda10f7470a45cf4f6
Philipp van Kempen <philipp.van-kempen@tum.de>
2021-03-21 03:08:11 +0100

    update

--- start_qemu.sh
+++ start_qemu.sh
@@ -9,5 +9,7 @@ touch $LOGFILE
 test -L $LOGDIR/current.log && rm $LOGDIR/current.log
 ln -s qemu_$TIMESTAMP.log $LOGDIR/current.log
 
+killall qemu-system-aarch64
 qemu/build/qemu-system-aarch64 -machine apple-m1 -nographic -serial pty -bios m1n1/build/m1n1.macho | tee $LOGFILE
+#qemu/build/qemu-system-aarch64 -machine apple-m1 -serial pty -bios m1n1/build/m1n1.macho | tee $LOGFILE
 
